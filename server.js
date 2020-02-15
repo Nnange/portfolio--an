fb7bcbc339195c33254ea955770c4c555ebde958
Nnange <47540685+Nnange@users.noreply.github.com>
2020-02-16 00:59:00 +0100

    few changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let app = express();
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 
  app.set('view engine', 'ejs');
- app.use(express.static('CSS'));
+ app.use(express.static('./public'));
 
  app.get('/', function(req, res){
     res.render('index');
